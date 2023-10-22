fe5760359343a650f0c72f2e09de84b1187331ae
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2023-10-21 21:42:01 -0300

    categories filters

--- src/components/ProductsTemplate/ProductsTemplate.tsx
+++ src/components/ProductsTemplate/ProductsTemplate.tsx
@@ -7,13 +7,15 @@ import ProductCard from "../ProductCard.tsx/ProductCard";
 import { Oval } from "react-loader-spinner";
 
 type PriceRange = [number, number];
-
+type Categories = string[];
 const ProductsTemplate = () => {
 
     const [products, setProducts] = useState<ProductItemProps[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [searchQuery, setSearchQuery] = useState<string>("");
     const [priceRange, setPriceRange] = useState<PriceRange>([0, 200]);
+    const [categories, setCategories] = useState<Categories>([]);
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
     const fetchProducts = async () => {
         setLoading(true);
@@ -28,26 +30,48 @@ const ProductsTemplate = () => {
         setLoading(false);
     };
 
+    const fetchCategories = async () => {
+        try {
+            const response = await axios.get(
+                `https://dummyjson.com/products/categories`
+            );
+            setCategories(response.data);
+        } catch (error) {
+            console.error('Error fetching product details:', error);
+        }
+    };
+
     useEffect(() => {
         fetchProducts();
+        fetchCategories();
     }, []);
 
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(event.target.value);
     };
 
-    
+
     const handlePriceRangeChange = (values: PriceRange) => {
         setPriceRange(values);
     };
 
+    const handleCategoryFilter = (category: string) => {
+        if (selectedCategories.includes(category)) {
+            const updatedCategories = selectedCategories.filter((cat) => cat !== category);
+            setSelectedCategories(updatedCategories);
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+    };
+
     const filteredProducts = products.filter((product) => {
-        if (product.price === undefined) return false;
-        return (
-            product.title.toLowerCase().includes(searchQuery.toLowerCase()) &&
-            product.price >= priceRange[0] &&
-            product.price <= priceRange[1]
-        );
+        if (product.price === undefined || product.category === undefined) return false;
+        const isInPriceRange = product.price >= priceRange[0] && product.price <= priceRange[1];
+        const isMatchSearchQuery = product.title.toLowerCase().includes(searchQuery.toLowerCase());
+        const isMatchCategory = selectedCategories.length
+            ? selectedCategories.includes(product.category)
+            : true;
+        return isInPriceRange && isMatchSearchQuery && isMatchCategory;
     });
 
     return (
@@ -89,7 +113,7 @@ const ProductsTemplate = () => {
                                 <input
                                     type="range"
                                     min={0}
-                                    max={1000}
+                                    max={2000}
                                     value={priceRange[0]}
                                     onChange={(e) =>
                                         handlePriceRangeChange([
@@ -102,7 +126,7 @@ const ProductsTemplate = () => {
                                 <input
                                     type="range"
                                     min={0}
-                                    max={1000}
+                                    max={2000}
                                     value={priceRange[1]}
                                     onChange={(e) =>
                                         handlePriceRangeChange([
@@ -113,10 +137,33 @@ const ProductsTemplate = () => {
                                     className="w-full"
                                 />
                             </div>
+                            <div>
+                                <h2>Categories</h2>
+                                <ul className="flex flex-col gap-1">
+                                    <li>
+                                        <input
+                                            type="checkbox"
+                                            onChange={() => handleCategoryFilter("all")}
+                                            checked={selectedCategories.length === categories.length}
+                                        />
+                                        <label className="capitalize">&nbsp;All</label>
+                                    </li>
+                                    {categories.map((category, index) => (
+                                        <li key={index}>
+                                            <input
+                                                type="checkbox"
+                                                onChange={() => handleCategoryFilter(category)}
+                                                checked={selectedCategories.includes(category)}
+                                            />
+                                            <label className="capitalize">&nbsp;{category}</label>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
                         </div>
                         <div className="grid grid-cols-12 gap-3 col-span-12 md:col-span-9">
                             {
-                                filteredProducts.map((product, index) => {
+                                filteredProducts.length > 0 ? (filteredProducts.map((product, index) => {
                                     return (
                                         <ProductCard
                                             key={index}
@@ -125,7 +172,11 @@ const ProductsTemplate = () => {
                                             reverse={true}
                                         />
                                     )
-                                })
+                                })) : (
+                                    <div className="col-span-12 md:col-span-9">
+                                        <h2 className="text-2xl">No Products Found</h2>
+                                    </div>
+                                )
                             }
                         </div>
                     </div>
