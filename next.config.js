9c8947dbf652338571bb1af2e58646ec05898562
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2023-10-28 19:36:17 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    images: {
-      domains: ['fakestoreapi.com', 'i.dummyjson.com', 'localhost'],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
   };
